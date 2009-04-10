873a131334235d0e340ec79a77afc9bcdfcb02b4
Brandon Fosdick <bfoz@bfoz.net>
2009-04-10 03:35:29 +0000

    New report columns that went into effect on April 1, 2009

--- perl5/tables.sql
+++ perl5/tables.sql
@@ -28,6 +28,10 @@ CREATE TABLE `dailySalesSummary` (
   `CustomerPrice` DECIMAL(10,2) NOT NULL,
   `CMA` CHAR(1),		-- Not used
   `AssetContentFlavor` CHAR(1),	-- Not used
+  `VendorOfferCode` VARCHAR(100),
+  `Grid` VARCHAR(18),
+  `PromoCode` VARCHAR(10),
+  `ParentIdentifier` VARCHAR(100),
   PRIMARY KEY `ID` (`ID`),
   UNIQUE KEY `VID_PTI_BeginDate_CountryCode` (`VendorIdentifier`(255),`ProductTypeIdentifier`,`BeginDate`,`CountryCode`(2))
 ) COMMENT='Daily Sales/Trend Summary Reports' AUTO_INCREMENT=1;
@@ -65,6 +69,10 @@ CREATE TABLE `weeklySalesSummary` (
   `CustomerPrice` DECIMAL(10,2) NOT NULL,
   `CMA` CHAR(1),		-- Not used
   `AssetContentFlavor` CHAR(1),	-- Not used
+  `VendorOfferCode` VARCHAR(100),
+  `Grid` VARCHAR(18),
+  `PromoCode` VARCHAR(10),
+  `ParentIdentifier` VARCHAR(100),
   UNIQUE KEY `VID_PTI_BeginDate_CountryCode` (`VendorIdentifier`(255),`ProductTypeIdentifier`,`BeginDate`,`CountryCode`(2))
 ) COMMENT='Weekly Sales/Trend Summary Reports';
 
@@ -74,30 +82,27 @@ CREATE TABLE `FinancialReport` (
   `RegionCode` CHAR(2) NOT NULL,    -- Not in the report, generated from the report filename
   `StartDate` DATE NOT NULL,
   `EndDate` DATE NOT NULL,
-  `VendorIdentifier` VARCHAR(255) NOT NULL,
-  `UPC` CHAR(1),			-- Not used
-  `ISRC` CHAR(1),			-- Not used
+  `UPC` VARCHAR(20),			-- Not used
+  `ISRC` VARCHAR(20),			-- Not used
+  `VendorIdentifier` VARCHAR(100) NOT NULL,
   `Quantity` INT,
-  `PartnerShare` DECIMAL(10,2) NOT NULL,
-  `ExtendedPartnerShare` DECIMAL(10,2) NOT NULL,
+  `PartnerShare` DECIMAL(18,2) NOT NULL,
+  `ExtendedPartnerShare` DECIMAL(18,2) NOT NULL,
   `PartnerShareCurrency` CHAR(3) NOT NULL,
-  `SaleOrReturn` CHAR(1) NOT NULL,
-  `AppleIdentifier` VARCHAR(255) NOT NULL,
-  `ArtistShowDeveloper` VARCHAR(255) NOT NULL,
-  `Title` VARCHAR(255) NOT NULL,
-  `LabelStudioNetwork` CHAR(1),		-- Not used
-  `ProductTypeIdentifier` TINYINT UNSIGNED NOT NULL,
-  `Side` CHAR(1),			-- Not used
-  `AccountIdentifier` VARCHAR(255) NOT NULL,
-  `CountryOfSale` VARCHAR(255) NOT NULL,
+  `SaleorReturn` CHAR(1) NOT NULL,
+  `AppleIdentifier` DECIMAL(18,0) NOT NULL,
+  `ArtistShowDeveloper` VARCHAR(4000) NOT NULL,
+  `Title` VARCHAR(5000) NOT NULL,
+  `LabelStudioNetworkDeveloper` VARCHAR(1000),		-- Not used
+  `Grid` VARCHAR(30),			-- Not used
+  `ProductTypeIdentifier` VARCHAR(5) UNSIGNED NOT NULL,
+  `ISANOtherIdentifier` VARCHAR(50),	-- Not used
+  `CountryOfSale` CHAR(3) NOT NULL,
   `PreorderFlag` CHAR(1),		-- Not used
-  `Prepaid` CHAR(1),			-- Not used
-  `PrepaidType` CHAR(1),		-- Not used
-  `ISANOtherIdentifier` CHAR(1),	-- Not used
-  `CMAFlag` CHAR(1),			-- Not used
-  `CustomerPrice` DECIMAL(10,2) NOT NULL,
+  `PromoCode` CHAR(10),
+  `CustomerPrice` DECIMAL(18,2) NOT NULL,
   `CustomerCurrency` CHAR(3) NOT NULL,
-  UNIQUE KEY `RID_RC_VID_PTI_CountryOfSale` (`ReportID`,`RegionCode`,`ProductTypeIdentifier`,`VendorIdentifier`(100),`CountryOfSale`(100),`CustomerPrice`)
+  UNIQUE KEY `RID_RC_VID_PTI_CountryOfSale` (`ReportID`,`RegionCode`,`ProductTypeIdentifier`,`VendorIdentifier`(100),`CountryOfSale`,`CustomerPrice`)
 ) COMMENT='Monthly Financial Reports' AUTO_INCREMENT=1;
 
 DROP TABLE IF EXISTS `applications`;
